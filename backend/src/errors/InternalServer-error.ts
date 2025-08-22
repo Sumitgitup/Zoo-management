46c4f649c625cb6b97973aafcecc66e79c2d2518
Aashish <aashishdubey.dev@gmail.com>
2025-08-22 08:17:38 +0530

    feat: added errorHandler middleware

---
+++ backend/src/errors/InternalServer-error.ts
@@ -0,0 +1,12 @@
+import { AppError } from "./AppError";
+import { StatusCodes } from "http-status-codes";
+
+export class InternalSeverError extends AppError {
+  constructor() {
+    super(
+      "INTERNAL_SERVER_ERROR",
+      StatusCodes.INTERNAL_SERVER_ERROR,
+      "Something happened on our side"
+    );
+  }
+}
