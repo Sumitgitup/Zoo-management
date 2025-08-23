1b42c8fb387fc13e105b3b69c4bd9fec2d659526
Prashant Singh <116258723+thefarziguy@users.noreply.github.com>
2025-08-23 17:52:33 +0530

    added enclosure and their sub-pages as well as updated routes and app.tsx with scrolltotop functionality

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -2,6 +2,9 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Animals from "./pages/Animals";
 import Enclosure from "./pages/Enclosure";
+import SafariZone from "./pages/eclosure-folder/SafariZone";
+import BirdSanctuary from "./pages/eclosure-folder/BirdSanctuary";
+import ReptileHouse from "./pages/eclosure-folder/ReptileHouse";
 
 const AppRoutes = () => {
   return (
@@ -9,6 +12,10 @@ const AppRoutes = () => {
       <Route path="/" element={<Home />} />
       <Route path="/animals" element={<Animals />} />
       <Route path="/enclosure" element={<Enclosure />} />
+      <Route path="/enclosure/safari-zone" element={<SafariZone />} />
+      <Route path="/enclosure/bird-sanctuary" element={<BirdSanctuary />} />
+      <Route path="/enclosure/reptile-house" element={<ReptileHouse />} />
+
       {/* <Route path="*" element={<Error_page />} /> */}
     </Routes>
   );
