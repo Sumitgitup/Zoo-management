8e3ad91979bfd55d2c0ffc02434a4023dec0d413
Sumitgitup <sumitroy7august@gmail.com>
2025-08-24 19:43:56 +0530

    feat: Add staff

--- backend/src/routes/v1/staff-route.ts
+++ backend/src/routes/v1/staff-route.ts
@@ -4,15 +4,17 @@ import {
   getStaffById,
   updateStaff,
   deleteStaff,
+  createStaff,
 } from "../../controllers/staff-controller";
 import validateResource from "../../middlewares/validate-input"; // Assuming you created this
 import { getStaffSchema, updateStaffSchema } from "../../types/staff";
+import { createAnimal } from "../../controllers/animal-controller";
 
 const router = Router();
 
 router.route("/")
 .get(validateResource(getStaffSchema), getStaff)
-
+.post(createStaff)
 
 router
   .route("/:id")
